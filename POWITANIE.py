0300e1d5e94e999de5ac7fe5d7d5bf9dfce916aa
DamianGitKonto <48293714+DamianGitKonto@users.noreply.github.com>
2021-08-10 02:26:23 +0200

    Add files via upload

---
+++ POWITANIE.py
@@ -0,0 +1,7 @@
+def Powitanie():
+    print("\n"
+    "Witaj w Zamku czarnoksiężnika. Zostałeś tu zesłany, przy użyciu magii. \n " \
+    "Twoim zadaniem jest opuścić w całości mury tego strasznego budynku. \n" \
+    "Niestety po drodze  spotkać Cię mogą liczne niebezpieczeństwa. \n" \
+    "POWODZENIA !\n"
+    "")
