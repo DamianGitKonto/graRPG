4f42b4ed3bb27652491735097a8d315a5f63b02a
DamianGitKonto <48293714+DamianGitKonto@users.noreply.github.com>
2021-08-10 02:25:59 +0200

    Add files via upload

---
+++ Lokacje.py
@@ -0,0 +1,4 @@
+# LOKACJE
+############################################################################################################
+def Lokacje ():
+    print(" Stoisz w wielkiej sali kolumnowej, jest tu dość jasno i bogato, na śwodku sali stoi Twój przeciwnik ")
