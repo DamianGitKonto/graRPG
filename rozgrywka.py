24b37eaed07b361bb2aee64b8e05a1b7bcd697f1
DamianGitKonto <48293714+DamianGitKonto@users.noreply.github.com>
2021-08-11 11:42:10 +0200

    Add files via upload

--- rozgrywka.py
+++ rozgrywka.py
@@ -1,80 +1,12 @@
 # Role Play Game
 
-# -*- coding: utf8 -*-
-
+# -*- coding: utf8 -*-w
 import POWITANIE
 import Lokacje
-
-POWITANIE.Powitanie()
-Lokacje.Lokacje()
+import Postacie
 
 p = ""
 
-# KLASY
-##########################################################################################
-
-class Mag:
-    name = ""
-    hp = int
-    power = int
-    mana = int
-    strength = int
-    spell1 = int
-    spell2 = int
-    level = int
-
-class Knight:
-    name = ""
-    hp = int
-    power = int
-    strength = int
-    attack1 = int
-    attack2 = int
-    level = int
-    armor = int
-
-# POSTACIE
-###########################################################################################
-
-obiekt1 = Mag()
-obiekt1.name = "Alfons"
-obiekt1.level = 1
-obiekt1.power = 10
-obiekt1.mana = 50
-obiekt1.strength = 20
-obiekt1.spell1 = 15
-obiekt1.spell2 = 15
-obiekt1.hp = 200 + obiekt1.level
-
-obiekt2 = Mag()
-obiekt2.name = "Girham"
-obiekt2.level = 1
-obiekt2.power = 10
-obiekt2.mana = 50
-obiekt2.strength = 20
-obiekt2.spell2 = 15
-obiekt2.spell2 = 15
-obiekt2.hp = 200 + obiekt2.level
-
-obiekt3 = Knight()
-obiekt3.name = "Eryck"
-obiekt3.level = 1
-obiekt3.power = 15
-obiekt3.armor = 50
-obiekt3.strength = 25
-obiekt3.attack1 = 25
-obiekt3.attack2 = 25
-obiekt3.hp = 200 + obiekt1.level * 0.10 + obiekt3.armor
-
-obiekt4 = Knight()
-obiekt4.name = "sir Roger"
-obiekt4.level = 1
-obiekt4.power = 15
-obiekt4.armor = 50
-obiekt4.strength = 25
-obiekt4.attack1 = 25
-obiekt4.attack2 = 25
-obiekt4.hp = 200 + obiekt1.level * 0.10 + obiekt3.armor
 
 
 p1 = input("Gracz 1. Wybierz postać \nA. Alfons \nB. Girham \nC. Erick \nD. sir Roger ")
@@ -103,6 +35,10 @@ elif p2 == "d":
      print("Gracz 1 wybrał sir Rogera \n")
 ####################################################################################################
 
+POWITANIE.Powitanie()
+
+Lokacje.Lokacje()
+Postacie.Postacie
 
 
 
