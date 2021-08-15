ff98b5f8632c5623492e1f3ba1c351fba8887e1c
DamianGitKonto <48293714+DamianGitKonto@users.noreply.github.com>
2021-08-15 20:30:25 +0200

    Add files via upload

--- Lokacje.py
+++ Lokacje.py
@@ -1,4 +1,11 @@
 # LOKACJE
 ############################################################################################################
-def Lokacje ():
-    print(" Stoisz w wielkiej sali kolumnowej, jest tu dość jasno i bogato, na śwodku sali stoi Twój przeciwnik ")
+story = {1: "Stoisz w małym ciemnym pomieszczeniu.\n"
+            "Na ścianach wiszą jakiś niewyraźne obrazy. \n"
+            "W rogu stoi niewielki stolik, obok przy ścianie proste drewniane łóżko.\n",
+         2: "Nagle! Z cienia wychodzi jakaś ledwo widoczna postać.\n"
+            "Zbliżając się powoli do Ciebie nieznacznym ruchem wysuwa miecz z pochwy. \n"
+            "Widzisz jak delikatnie błyska ostrze niecza. \n"
+            "Wygląda na to że walka lub ucieczka są nieuniknione",
+         3:"i"
+         }
